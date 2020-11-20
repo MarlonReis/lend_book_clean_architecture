d1e19318e9d470e7ed029eadde6b347953f66b58
Marlon Reis <marlon.reis@smartmei.com.br>
2020-11-20 17:26:32 -0300

    test: name must contain at least  characters

---
+++ test/domain/object-value/Name.spec.ts
@@ -0,0 +1,19 @@
+import InvalidNameError from '../../../src/domain/errors/InvalidNameError'
+import { Name } from '../../../src/domain/object-value/Name'
+
+describe('Name', () => {
+  test('should be create valid name', () => {
+    const eitherName = Name.create('Any Name')
+
+    expect(eitherName.isSuccess()).toBe(true)
+    expect(eitherName.value).toBeInstanceOf(Name)
+  })
+
+  test('should return failure when nome have only throw characters', () => {
+    const eitherName = Name.create('An')
+
+    expect(eitherName.isSuccess()).toBe(false)
+    expect(eitherName.isFailure()).toBe(true)
+    expect(eitherName.value).toBeInstanceOf(InvalidNameError)
+  })
+})
