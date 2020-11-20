c80b13c70f0cd0eb3f7ac3fe4d68322e5053c3e8
Marlon Reis <marlon.reis@smartmei.com.br>
2020-11-20 16:51:40 -0300

    test: return valid name

--- test/domain/object-value/name.spec.ts
+++ test/domain/object-value/name.spec.ts
@@ -2,7 +2,9 @@ import { Name } from '../../../src/domain/object-value/Name'
 
 describe('Name', () => {
   test('should be create valid name', () => {
-    const name = new Name('Any Name')
-    expect(name.value).toBe('Any Name')
+    const eitherName = Name.create('Any Name')
+
+    expect(eitherName.isSuccess()).toBe(true)
+    expect(eitherName.value).toBeInstanceOf(Name)
   })
 })
