96a0e6bf6c9d0ce8ccb2c8b77d43be5f930f683d
Marlon Reis <marlon.reis@smartmei.com.br>
2020-11-20 18:47:17 -0300

    test: create emaio failure when email is invalid

--- test/domain/object-value/Email.spec.ts
+++ test/domain/object-value/Email.spec.ts
@@ -1,3 +1,4 @@
+import InvalidEmailError from '../../../src/domain/errors/InvalidEmailError'
 import Email from '../../../src/domain/object-value/Email'
 
 describe('Email', () => {
@@ -5,5 +6,12 @@ describe('Email', () => {
         const result = Email.create('valid@email.com.br')
         expect(result.isSuccess()).toBe(true)
         expect(result.value).toBeInstanceOf(Email)
+    })
+
+    test('should failure is true when email is invalid', () => {
+        const result = Email.create('invalid-email.com.br')
+        expect(result.isSuccess()).toBe(false)
+        expect(result.isFailure()).toBe(true)
+        expect(result.value).toBeInstanceOf(InvalidEmailError)
     })
 })
