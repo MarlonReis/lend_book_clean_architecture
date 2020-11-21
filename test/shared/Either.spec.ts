7d3c4d19b454f07c392f12b35be6542c2ad9b3ca
Marlon Reis <marlon.reis@smartmei.com.br>
2020-11-21 13:20:55 -0300

    test: return error when call failure function

--- test/shared/Either.spec.ts
+++ test/shared/Either.spec.ts
@@ -1,11 +1,25 @@
-import { success } from '../../src/shared/Either'
+import { failure, success } from '../../src/shared/Either'
 
 describe('Either', () => {
     test('should return object when call success function', () => {
         const response = success({ data: 'valid value' })
+
         expect(response.isSuccess()).toBe(true)
+        expect(response.isFailure()).toBe(false)
+        expect(response.value).toBeInstanceOf(Object)
         expect(response.value).toMatchObject({
-            data: expect.anything()
+            data: 'valid value'
+        })
+    })
+
+    test('should return error when call failure function', () => {
+        const response = failure(new Error('message error'))
+
+        expect(response.isSuccess()).toBe(false)
+        expect(response.isFailure()).toBe(true)
+        expect(response.value).toBeInstanceOf(Error)
+        expect(response.value).toMatchObject({
+            message: 'message error'
         })
     })
 })
