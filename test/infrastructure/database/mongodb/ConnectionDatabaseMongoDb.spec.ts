1205f49f045717cff7ba33936e942de5c56dd4b8
Marlon Reis <marlon.reis@smartmei.com.br>
2020-11-23 11:17:43 -0300

    test: get collection by name

--- test/infrastructure/database/mongodb/ConnectionDatabaseMongoDb.spec.ts
+++ test/infrastructure/database/mongodb/ConnectionDatabaseMongoDb.spec.ts
@@ -1,3 +1,4 @@
+import { Collection } from 'mongodb'
 import { ConnectionDatabaseMongoDb } from '../../../../src/infrastructure/database/mongodb/ConnectionDatabaseMongoDb'
 
 const connection = new ConnectionDatabaseMongoDb(process.env.MONGO_URL)
@@ -8,5 +9,11 @@ describe('ConnectionDatabaseMongoDb', () => {
 
     test('should be open connection with database in memory', async () => {
         expect(connection.isConnected()).toBe(true)
+    })
+
+    test('should get collection by name', async () => {
+        const collection: Collection = await connection.getCollectionByName('account')
+        expect(collection).toBeTruthy()
+        expect(collection.collectionName).toBe('account')
     })
 })
