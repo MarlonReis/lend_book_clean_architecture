267f1d900eb4b23622922184b3d32c9814381eed
Marlon Reis <marlon.reis@smartmei.com.br>
2020-11-23 14:29:47 -0300

    test: return failure when bcrypt throws error

--- src/infrastructure/adapter/BCryptEncryptsPasswordAdapter.ts
+++ src/infrastructure/adapter/BCryptEncryptsPasswordAdapter.ts
@@ -5,9 +5,15 @@ import { Password } from '../../domain/object-value'
 import { Either, failure } from '../../shared/Either'
 
 export class BCryptEncryptsPasswordAdapter implements EncryptsPassword {
+    private readonly saltOrRounds: number
+
+    constructor (saltOrRounds: number) {
+        this.saltOrRounds = saltOrRounds
+    }
+
     encrypt = async (password: Password): Promise<Either<InvalidPasswordError, Password>> => {
         try {
-            const passwordEncrypted = await bcrypt.hash(password.value, 12)
+            const passwordEncrypted = await bcrypt.hash(password.value, this.saltOrRounds)
             return Password.create(passwordEncrypted)
         } catch (err) {
             return failure(new InvalidPasswordError(password.value))
--- test/infrastructure/adapter/BCryptEncryptsPasswordAdapter.spec.ts
+++ test/infrastructure/adapter/BCryptEncryptsPasswordAdapter.spec.ts
@@ -1,21 +1,31 @@
+import bcrypt from 'bcrypt'
 import { EncryptsPassword } from '../../../src/data/protocol/EncryptsPassword'
 import { Password } from '../../../src/domain/object-value'
 import { BCryptEncryptsPasswordAdapter } from '../../../src/infrastructure/adapter/BCryptEncryptsPasswordAdapter'
 
 describe('BCryptEncryptsPasswordAdapter', () => {
+    let encrypt: EncryptsPassword
+
+    beforeAll(() => {
+        encrypt = new BCryptEncryptsPasswordAdapter(12)
+    })
+
     test('should encrypt password is valid', async () => {
-        const encrypt: EncryptsPassword = new BCryptEncryptsPasswordAdapter()
-        const passwordOrError = Password.create('P4ssW0rd@Valid')
-
-        if (passwordOrError.isFailure()) {
-            fail("Shouldn't have entered this conditional")
-        }
-
-        const password: Password = passwordOrError.value
+        const password: Password = { value: 'P4ssW0rd@Valid' }
         const passwordEncrypted = await encrypt.encrypt(password)
 
         expect(passwordEncrypted.value).toMatchObject({
             value: expect.any(String)
         })
+    })
+
+    test('should return failure when bcrypt throws error', async () => {
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+            throw new Error('Any error')
+        })
+
+        const password: Password = { value: 'P4ssW0rd@Valid' }
+        const passwordEncrypted = await encrypt.encrypt(password)
+        expect(passwordEncrypted.isFailure()).toBe(true)
     })
 })
