c8418bc698767b2dd3aa37fb0e1eccde6cc32ae7
Marlon Reis <marlon.reis@smartmei.com.br>
2020-11-24 10:24:26 -0300

    feat: return failure when make error in mongo repository

--- src/infrastructure/database/mongodb/repositories/CreateUserAccountMongoRepository.ts
+++ src/infrastructure/database/mongodb/repositories/CreateUserAccountMongoRepository.ts
@@ -1,9 +1,13 @@
 import { CreateUserAccountRepository } from '../../../../data/repositories/CreateUserAccountRepository'
 import { CreateUserAccountError } from '../../../../domain/errors'
 import { User } from '../../../../domain/model/user/User'
-import { Either, success } from '../../../../shared/Either'
+import { Either, failure, success } from '../../../../shared/Either'
 import { ConnectionDatabaseMongoDb } from '../connection/ConnectionDatabaseMongoDb'
 
+const mapResponse = (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection
+    return { id: { value: _id }, ...collectionWithoutId }
+}
 export class CreateUserAccountMongoRepository implements CreateUserAccountRepository {
     private readonly mongoConnection: ConnectionDatabaseMongoDb
 
@@ -12,20 +16,16 @@ export class CreateUserAccountMongoRepository implements CreateUserAccountReposi
     }
 
     create = async (user: User): Promise<Either<CreateUserAccountError, User>> => {
-        const accountCollection = await this.mongoConnection.getCollectionByName('user_account')
-        const response = await accountCollection.insertOne(
-            {
+        try {
+            const accountCollection = await this.mongoConnection.getCollectionByName('user_account')
+            const response = await accountCollection.insertOne({
                 name: user.getName(),
                 email: user.getEmail(),
                 password: user.getPassword()
-            }
-        )
-
-        return success(this.map(response.ops[0]))
-    }
-
-    private map (collection: any): any {
-        const { _id, ...collectionWithoutId } = collection
-        return { id: { value: _id }, ...collectionWithoutId }
+            })
+            return success(mapResponse(response.ops[0]))
+        } catch (err) {
+            return failure(new CreateUserAccountError(err))
+        }
     }
 }
