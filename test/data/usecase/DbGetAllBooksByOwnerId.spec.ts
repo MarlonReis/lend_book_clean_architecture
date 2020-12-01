2fc5f2403223284747354b64d751310b7e39d39d
Marlon Reis <marlon.reis@smartmei.com.br>
2020-12-01 16:11:44 -0300

    test: return all books by owner id

---
+++ test/data/usecase/DbGetAllBooksByOwnerId.spec.ts
@@ -0,0 +1,31 @@
+import { GetAllBooksByOwnerIdRepository } from '../../../src/data/repositories/GetAllBooksByOwnerIdRepository'
+import { DbGetAllBooksByOwnerId } from '../../../src/data/usecase/DbGetAllBooksByOwnerId'
+import { NotFoundError } from '../../../src/domain/errors'
+import { Book } from '../../../src/domain/model/book/Book'
+import { IdEntity } from '../../../src/domain/object-value'
+import { Either, success } from '../../../src/shared/Either'
+
+describe('GetAllBooksByOwnerId', () => {
+    test('should get all books by owner id', async () => {
+        class GetAllBooksByOwnerIdRepositoryStub implements GetAllBooksByOwnerIdRepository {
+            getByOwnerId = async (id: IdEntity): Promise<Either<NotFoundError, Book[]>> => {
+                const bookOrError = Book.create({
+                    title: 'Any Title',
+                    owner: {
+                        email: 'any@email.com.br',
+                        name: 'Any Name',
+                        password: 'Password@Valid'
+                    }
+                })
+                return await Promise.resolve(success([bookOrError.value as Book]))
+            }
+        }
+        const repository = new GetAllBooksByOwnerIdRepositoryStub()
+        const sut = new DbGetAllBooksByOwnerId(repository)
+        const id = IdEntity.create('valid-id').value as IdEntity
+        const response = await sut.getByOwnerId(id)
+
+        expect(response.isSuccess()).toBe(true)
+        expect(response.value).toBeInstanceOf(Array)
+    })
+})
