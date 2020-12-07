95924072d67e1380e58c9a66e2c651cbe72aa314
Marlon Reis <marlon.reis@smartmei.com.br>
2020-12-07 14:51:45 -0300

    refactor: remove id validation

--- src/presentation/controller/GetAllBooksByOwnerIdController.ts
+++ src/presentation/controller/GetAllBooksByOwnerIdController.ts
@@ -1,5 +1,5 @@
 import { GetAllBooksByOwnerId } from '../../domain/usecase/GetAllBooksByOwnerId'
-import { badRequest, internalServerError } from '../helper/HttpResponseHelper'
+import { internalServerError } from '../helper/HttpResponseHelper'
 import { Controller } from '../protocol/Controller'
 import { HttpRequest, HttpResponse } from '../protocol/Http'
 
@@ -12,10 +12,6 @@ export class GetAllBooksByOwnerIdController implements Controller {
 
     handle = async (request: HttpRequest): Promise<HttpResponse> => {
         const { id } = request.params
-
-        if (!id) {
-            return badRequest(new Error("params 'id' required!"))
-        }
 
         const response = await this.getAllBooksByOwnerId.getByOwnerId(id)
         if (response.isFailure()) {
