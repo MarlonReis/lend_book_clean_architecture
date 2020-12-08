48637c683193943645e4bc1ea3a4063f91e72234
Marlon Reis <marlon.reis@smartmei.com.br>
2020-12-08 10:13:49 -0300

    refactor: move class creation to a factory method

--- test/presentation/controller/GetAllBooksByOwnerIdController.spec.ts
+++ test/presentation/controller/GetAllBooksByOwnerIdController.spec.ts
@@ -2,23 +2,35 @@ import { NotFoundError } from '../../../src/domain/errors'
 import { Book } from '../../../src/domain/model/Book'
 import { GetAllBooksByOwnerId } from '../../../src/domain/usecase/GetAllBooksByOwnerId'
 import { GetAllBooksByOwnerIdController } from '../../../src/presentation/controller/GetAllBooksByOwnerIdController'
+import { Controller } from '../../../src/presentation/protocol/Controller'
 import { Either, success } from '../../../src/shared/Either'
+
+const makeGetAllBooksByOwnerIdFactory = (): GetAllBooksByOwnerId => {
+    class GetAllBooksByOwnerIdStub implements GetAllBooksByOwnerId {
+        async getByOwnerId (id: string): Promise<Either<NotFoundError, Book[]>> {
+            const book = Book.create({
+                id: '3baa191c-364e-11eb-adc1-0242ac120003',
+                title: 'Any title',
+                ownerId: '3baa191c-364e-11eb-adc1-0242ac120002'
+            })
+            return await Promise.resolve(success([book.value as Book]))
+        }
+    }
+    return new GetAllBooksByOwnerIdStub()
+}
+interface TypeSut {
+    sut: Controller
+    getAllBooksByOwnerIdSut: GetAllBooksByOwnerId
+}
+const makeSutFactory = (): TypeSut => {
+    const getAllBooksByOwnerIdSut = makeGetAllBooksByOwnerIdFactory()
+    const sut = new GetAllBooksByOwnerIdController(getAllBooksByOwnerIdSut)
+    return { sut, getAllBooksByOwnerIdSut }
+}
 
 describe('GetAllBooksByOwnerIdController', () => {
     test('should return success when found book', async () => {
-        class GetAllBooksByOwnerIdSut implements GetAllBooksByOwnerId {
-            async getByOwnerId (id: string): Promise<Either<NotFoundError, Book[]>> {
-                const book = Book.create({
-                    id: '3baa191c-364e-11eb-adc1-0242ac120003',
-                    title: 'Any title',
-                    ownerId: '3baa191c-364e-11eb-adc1-0242ac120002'
-                })
-                return await Promise.resolve(success([book.value as Book]))
-            }
-        }
-
-        const getAllBooksByOwnerIdSut = new GetAllBooksByOwnerIdSut()
-        const sut = new GetAllBooksByOwnerIdController(getAllBooksByOwnerIdSut)
+        const { sut } = makeSutFactory()
         const response = await sut.handle({
             params: { id: 'valid-id' }
         })
