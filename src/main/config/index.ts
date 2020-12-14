c010f82330d29e1fb14391c90cdea6c710103e24
MarlonReis <marlon-reis91@hotmail.com>
2020-12-14 11:53:32 -0300

    refactor: change database connection

--- src/main/config/index.ts
+++ src/main/config/index.ts
@@ -1,5 +1,5 @@
 
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://admin:MongoDB2019@ds161505.mlab.com:61505/clean-node-api',
+    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://clean:P4ssw0rd3x3mpl3@cluster0.plget.mongodb.net/example?retryWrites=true&w=majority',
     port: process.env.PORT || 8080
 }
