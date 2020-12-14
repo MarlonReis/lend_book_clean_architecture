7f351ebaff5d117eb4c9827b0bf424c1ca7644e9
MarlonReis <marlon-reis91@hotmail.com>
2020-12-14 11:56:00 -0300

    refactor: call get all books by owner by id resolver

--- src/main/graphql/config/App.ts
+++ src/main/graphql/config/App.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import Express from 'express'
-import { AccountResolver } from '../resolver/account/AccountResolver'
 import { GraphQLSchema } from 'graphql'
 
+import { AccountResolver } from '../resolver/account/AccountResolver'
+import { GetAllBooksByOwnerIdResolver } from '../resolver/books/GetAllBooksByOwnerIdResolver'
+
 const createSchema = async (): Promise<GraphQLSchema> => await buildSchema({
-    resolvers: [AccountResolver]
+    resolvers: [AccountResolver, GetAllBooksByOwnerIdResolver]
 })
 
 export const createGraphqlSchema = async (): Promise<GraphQLSchema> => await createSchema()
