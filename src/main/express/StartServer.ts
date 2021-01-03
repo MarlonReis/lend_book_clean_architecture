351613c3eb43b9f8ca5eea77b0a4aa6d661cbb63
Marlon Gonçalves Reis <marlon.reis@smartmeil.com.br>
2021-01-03 02:36:02 -0300

    feat: add module alias

--- src/main/express/StartServer.ts
+++ src/main/express/StartServer.ts
@@ -1,8 +1,10 @@
+import 'module-alias/register'
+
 import app from './config/App'
 import { ConnectionDatabaseMongoDb } from '@/infrastructure/database/mongodb/connection/ConnectionDatabaseMongoDb'
 import config from '@/main/config'
 
 ConnectionDatabaseMongoDb.getInstance().open(config.mongoUrl)
     .then(() => {
-        app.listen(config.port, () => console.log(`Server running at http://localhost:${config.port}`))
+        app.listen(config.port, () => console.log(`🚀 Server running at http://localhost:${config.port}`))
     }).catch(err => console.log(err.message))
